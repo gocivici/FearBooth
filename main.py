4be8c998b5d09820ca300cca4a58e60b94378746
gocivici <gocivici@gmail.com>
2023-10-27 14:53:59 +0300

    fixe cv2 error

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ if cam.isOpened():
                 if TIMER>0:
                     draw.text((130, 300), str(TIMER), font=fontBIG,fill=(255,0,0,255))
                 else:
-                    cv2.imwrite("Fear_{}.jpg".format(prev))
+                    cv2.imwrite("Fear_{}.jpg".format(prev),img)
                     draw.text((30, 480), "ANALYZING...", font=fontSmall,fill=(255,0,0,255))
                 img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
                 img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
